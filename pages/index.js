8be7ab60f5a84cd87dc519486d3238e997432ace
madhur7sharma <madhur07sharma@gmail.com>
2022-05-22 20:47:55 +0530

    design

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
     const [good, setGood] = useState("");
     const [bad, setBad] = useState("");
     const [filteredArr, setFilteredArr] = useState(null);
+    const [warn, setWarn] = useState(false);
     const dataObj = {
         one: one,
         two: two,
@@ -28,60 +29,107 @@ export default function Home() {
         if (key === "good") {
             if (!bad.includes(lastLetter)) {
                 setGood(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "bad") {
             if (!good.includes(lastLetter) && one !== lastLetter && two !== lastLetter && three !== lastLetter && four !== lastLetter && five !== lastLetter) {
                 setBad(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "one") {
             if (!bad.includes(lastLetter)) {
                 setOne(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "two") {
             if (!bad.includes(lastLetter)) {
                 setTwo(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "three") {
             if (!bad.includes(lastLetter)) {
                 setThree(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "four") {
             if (!bad.includes(lastLetter)) {
                 setFour(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
         if (key === "five") {
             if (!bad.includes(lastLetter)) {
                 setFive(value);
+                setWarn(false);
+            } else {
+                setWarn(true);
             }
         }
     };
     return (
-        <div className="flex flex-col items-center justify-center w-screen my-8">
+        <div className="flex flex-col items-center justify-center w-screen my-8 ">
             <Head>
                 <title>Wordle Finder</title>
             </Head>
-            <h1 className="text-4xl mb-6 font-bold">Wordle Finder</h1>
-            <div className="bg-lime-400 w-11/12 max-w-[500px] p-8 flex flex-col h-full">
+            <h1 className="text-4xl mb-6 font-bold text-white">Wordle Finder</h1>
+            <div className="bg-gray-500 w-11/12 max-w-[500px] md:p-8 p-5 flex flex-col h-full rounded">
                 <div className="flex flex-col gap-4">
                     <input placeholder="Good Letters" value={good} onChange={(e) => validate(e.target.value.toUpperCase(), "good")} className="h-10 pl-4 text-xl" />
                     <input placeholder="Bad Letters" value={bad} onChange={(e) => validate(e.target.value.toUpperCase(), "bad")} className="h-10 pl-4 text-xl" />
                 </div>
-                <div className="bg-gray-500 p-4 grid grid-cols-5 place-items-center mt-4">
-                    <input value={one} onChange={(e) => validate(e.target.value.toUpperCase(), "one")} maxLength="1" className="w-12 h-12 text-center text-3xl" />
-                    <input value={two} onChange={(e) => validate(e.target.value.toUpperCase(), "two")} maxLength="1" className="w-12 h-12 text-center text-3xl" />
-                    <input value={three} onChange={(e) => validate(e.target.value.toUpperCase(), "three")} maxLength="1" className="w-12 h-12 text-center text-3xl" />
-                    <input value={four} onChange={(e) => validate(e.target.value.toUpperCase(), "four")} maxLength="1" className="w-12 h-12 text-center text-3xl" />
-                    <input value={five} onChange={(e) => validate(e.target.value.toUpperCase(), "five")} maxLength="1" className="w-12 h-12 text-center text-3xl" />
+                <div className="bg-gray-800 md:p-4 p-2 grid grid-cols-5 place-items-center mt-4">
+                    <input
+                        value={one}
+                        onChange={(e) => validate(e.target.value.toUpperCase(), "one")}
+                        maxLength="1"
+                        className="w-12 h-12 text-center text-3xl border-[3px] border-yellow-500"
+                    />
+                    <input
+                        value={two}
+                        onChange={(e) => validate(e.target.value.toUpperCase(), "two")}
+                        maxLength="1"
+                        className="w-12 h-12 text-center text-3xl border-[3px] border-yellow-500"
+                    />
+                    <input
+                        value={three}
+                        onChange={(e) => validate(e.target.value.toUpperCase(), "three")}
+                        maxLength="1"
+                        className="w-12 h-12 text-center text-3xl border-[3px] border-yellow-500"
+                    />
+                    <input
+                        value={four}
+                        onChange={(e) => validate(e.target.value.toUpperCase(), "four")}
+                        maxLength="1"
+                        className="w-12 h-12 text-center text-3xl border-[3px] border-yellow-500"
+                    />
+                    <input
+                        value={five}
+                        onChange={(e) => validate(e.target.value.toUpperCase(), "five")}
+                        maxLength="1"
+                        className="w-12 h-12 text-center text-3xl border-[3px] border-yellow-500"
+                    />
                 </div>
+                <div className="w-fit h-8 text-center mx-auto">{warn && <p className="text-yellow-500">Good letters and bad letters cannot be same!!</p>}</div>
                 <button
                     onClick={filterWords}
-                    className="mt-8 p-2 px-4 bg-gray-800 text-white border-2 border-transparent hover:border-yellow-500 hover:bg-gray-600 hover:text-yellow-500 transition-all"
+                    className="p-2 px-4 bg-gray-800 text-white border-2 border-transparent hover:border-yellow-500 hover:bg-gray-700 hover:text-yellow-500 transition-all"
                 >
                     Find Words
                 </button>
@@ -89,7 +137,7 @@ export default function Home() {
                     <div className="flex gap-2 flex-wrap mt-6">
                         {filteredArr.map((item, pos) => {
                             return (
-                                <span key={pos} className="bg-gray-200 p-2 font-medium">
+                                <span key={pos} className="bg-gray-200 md:p-2 p-1 font-medium border border-yellow-500">
                                     {item.toUpperCase()}
                                 </span>
                             );
